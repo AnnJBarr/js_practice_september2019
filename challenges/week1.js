c0666cdeed8770e64378fd043e4da7354b77aa98
AnnJBarr <annbarr@hotmail.com>
2019-09-30 12:21:52 +0100

    countLinuxUsers completed

--- challenges/week1.js
+++ challenges/week1.js
@@ -15,25 +15,25 @@ function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
-  return Math.round(originalPrice*100 +(originalPrice*vatRate))/100;
+  return Math.round(originalPrice * 100 + (originalPrice * vatRate)) / 100;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   // Add your code here!
-  return Math.round(originalPrice*100-(originalPrice*reduction))/100;
+  return Math.round(originalPrice * 100 - (originalPrice * reduction)) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
-  let mid = str.length/2;
-  if (str.length % 2 === 0){
-    return str[mid-1].concat(str[mid]);
-    } else {
-    return str[Math.round(mid)-1];
-    }
+  let mid = str.length / 2;
+  if (str.length % 2 === 0) {
+    return str[mid - 1].concat(str[mid]);
+  } else {
+    return str[Math.round(mid) - 1];
+  }
 }
 
 function reverseWord(word) {
@@ -46,17 +46,25 @@ function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
   const reversedWords = [];
-  for (let i=0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     let word = words[i];
     let reverseWord = word.split("").reverse().join("");
     reversedWords.push(reverseWord);
-    }
+  }
   return reversedWords
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
+  let count = 0;
+  for (let key in users) {
+    const value = users[key];
+    if (value.type === "Linux") {
+      count++;
+    }
+  }
+  return count;
 }
 
 function getMeanScore(scores) {
