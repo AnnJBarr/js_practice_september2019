9985fc3fe11bcfa8883dfc778bc71ba0f993770d
AnnJBarr <annbarr@hotmail.com>
2019-10-04 09:57:42 +0100

    fn isFromManchester completed

--- challenges/week2.js
+++ challenges/week2.js
@@ -7,6 +7,11 @@ function getFillings(sandwich) {
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.city === "Manchester") {
+    return true
+  } else {
+    return false
+  }
 }
 
 function getBusNumbers(people) {
